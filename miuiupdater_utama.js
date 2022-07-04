4e209698b941b46fd9e7cc5a96b8e6fca147b86a
VipTrikID <32627824+VipTrikID@users.noreply.github.com>
2022-07-04 18:58:41 +0700

    Update miuiupdater_utama.js

--- miuiupdater_utama.js
+++ miuiupdater_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="how-to-get-development-finance.html"}
 if(x=="2"){xxx="points-to-analyse-about-business-loan.html"}
 if(x=="3"){xxx="the-real-difference-between-marketing.html"}
-if(x=="4"){xxx="strategic-customer-attraction-marketing.html"}a_to_ve[i].href="http://www.miuiupdater.tech/2018/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="strategic-customer-attraction-marketing.html"}a_to_ve[i].href="http://miuiupdatersafer.blogspot.com//2018/03/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
